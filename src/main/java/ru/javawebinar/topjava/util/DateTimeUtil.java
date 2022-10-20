5da64527631fd3aff3da63bfa933e77bf41c3b1b
Igor Kraev <ivk.spb@yandex.ru>
2022-10-20 14:30:07 +0300

    HW02: code refactoring

--- src/main/java/ru/javawebinar/topjava/util/DateTimeUtil.java
+++ src/main/java/ru/javawebinar/topjava/util/DateTimeUtil.java
@@ -4,20 +4,21 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
 
 public class DateTimeUtil {
     private static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
 
-    public static boolean isBetweenHalfOpen(LocalTime lt, LocalTime startTime, LocalTime endTime) {
-        return (startTime == null || lt.compareTo(startTime) >= 0) && (endTime == null || lt.compareTo(endTime) < 0);
+    public static <T extends Comparable<T>> boolean isBetweenHalfOpen(T value, T start, T end) {
+        return (start == null || value.compareTo(start) >= 0) && (end == null || value.compareTo(end) < 0);
     }
 
-    public static LocalDate getStartDateOrMin(LocalDate localDate) {
-        return localDate == null ? LocalDate.of(1, 1, 1) : localDate.minusDays(1);
+    public static LocalDateTime getStartDateOrMin(LocalDate localDate) {
+        return localDate == null ? LocalDateTime.of(LocalDate.MIN, LocalTime.MIN) : localDate.atStartOfDay();
     }
 
-    public static LocalDate getEndDateOrMax(LocalDate localDate) {
-        return localDate == null ? LocalDate.of(5000, 1, 1) : localDate.plusDays(1);
+    public static LocalDateTime getEndDateOrMax(LocalDate localDate) {
+        return localDate == null ? LocalDateTime.of(LocalDate.MAX, LocalTime.MAX) : localDate.plus(1, ChronoUnit.DAYS).atStartOfDay();
     }
 
     public static LocalDate parseLocalDate(String str) {
